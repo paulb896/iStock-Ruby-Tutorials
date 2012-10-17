f56ee2b85d6ea6647e03c455f8f10d8e4bd7f97c
paulb896 <paulb896@gmail.com>
2012-10-17 13:17:41 -0600

    Description:
      Add base rails application
      Add ability to create articles
      Add ability to add comments to articles
      Add ability to add photos to an article
      Add ability to tag articles
      Add ability to manage tags
      Add user creation/login

---
+++ tutorial2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tutorial2::Application.config.secret_token = 'c4a753cab82a534fafb29b9566c46ec7d13a2918eaa3db41e857619eddba412d8437ef4e1602fe469cf9e174079a7595871097dc1b751b6d377489084531411d'
---
+++ tutorial2/config/routes.rb
@@ -0,0 +1,18 @@
+Tutorial2::Application.routes.draw do
+  resources :authors
+
+  root :to => 'articles#index'
+  get "tags/index"
+
+  get "tags/show"
+
+  resources :articles
+  resources :comments
+  resources :tags
+  
+  resources :author_sessions
+
+  match 'login'  => 'author_sessions#new',     :as => :login
+  match 'logout' => 'author_sessions#destroy', :as => :logout
+
+end
