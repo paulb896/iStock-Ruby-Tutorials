0019da11aa0f602a31aca9e5b7124d7106cb7745
paulb896 <paulb896@gmail.com>
2012-10-18 10:06:25 -0600

    Description:   Add security to verify that a user is logged in before adding/editing content

--- tutorial2/app/controllers/articles_controller.rb
+++ tutorial2/app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  before_filter :require_login, :except => [:index, :show]
+
   def index
     @articles = Article.all
   end
--- tutorial2/app/controllers/comments_controller.rb
+++ tutorial2/app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+
+  before_filter :require_login, :except => [:create]
+
   def create
     article_id = params[:comment].delete(:article_id)
     @comment = Comment.new(params[:comment])
