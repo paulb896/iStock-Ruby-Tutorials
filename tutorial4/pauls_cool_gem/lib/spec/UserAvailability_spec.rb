9389bb1ed978a55c1d9d350084a899f90bab4ee9
paulb896 <paulb896@gmail.com>
2012-11-02 15:57:19 -0600

    Add RSpec test to verify UserAvailablity::is_available? function

--- tutorial4/pauls_cool_gem/lib/spec/UserAvailability_spec.rb
+++ tutorial4/pauls_cool_gem/lib/spec/UserAvailability_spec.rb
@@ -9,12 +9,27 @@ describe PaulsCoolGem::UserAvailability do
   end
 
   describe "#request_time" do
-    it "returns booking request status" do
+    it "returns booking request status of pending when requesting an available date" do
       @scheduler.request_time("Test User", @start_datetime, @end_datetime).should eq('Pending')
 
+    end
+
+    it "returns booking request status of pending when requesting a non-available date" do
       invalid_start = @start_datetime >> 2
       invalid_end = @end_datetime >> 2
       @scheduler.request_time("Test User", invalid_start, invalid_end).should eq('Declined')
+    end
+  end
+
+  describe "#is_available?" do
+    it "returns false when user time is not available" do
+      @scheduler.is_available?(@start_datetime, @end_datetime).should eq(false)
+    end
+
+    it "returns true when user time is available" do
+      available_start = @start_datetime >> 2
+      available_end = @end_datetime >> 2
+      @scheduler.is_available?(available_start, available_end).should eq(true)
     end
   end
 end
